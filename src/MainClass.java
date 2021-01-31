5ec3a073b5d63a28e64a6432ef681403bf39500d
SRPKseni <51365325+KseniSRP@users.noreply.github.com>
2021-01-31 12:21:15 +0300

    adding files

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,12 @@
 
 public class MainClass {
-    public int getLocalNumber(){
-        return 14;
+    public int getLocalNumber() {
+       return 14;
+   }
+
+    public int getClassNumber () {
+        int class_number = 20;
+        return class_number;
     }
 }
+
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,6 +8,15 @@ public class MainClassTest extends MainClass {
         System.out.println("этот тест вернул значение и оно равно " + a);
         } else { System.out.println( "что-то пошло не так");
          }
-
+   }
+    @Test
+    public void testGetClassNumber () {
+        int a;
+        a = getClassNumber();
+        if (a > 45) {
+            System.out.println("Значение параметра  а больше 45б а именно a=" + a);
+        } else {
+            System.out.println("Значение парамерта а меньше 45 (или = 45), а именно a=" + a);
+        }
     }
 }
